68dd3a87d51edf7fe267d9bc798f15fc60a26045
Janis Nguenang <janis.nguenang@mirakl.com>
2017-09-12 10:46:36 +0200

    Insert-in-table
    These 2 scripts: "insert_domain-name.sh" and "record-dns_miraklnet.sh" allow to insert some informations (extract in the file "route_53-miraklnet.txt") in the 2 tables "domain" and "record".

---
+++ script-insertSQL/record-dns_miraklnet.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+FILE='/docker-entrypoint-initdb.d/route_53-miraklnet.txt';
+
+DB_USER='janis';
+DB_PASSWD='root';
+
+DOMAIN_ID=$(mysql --user=$DB_USER --password=$DB_PASSWD -s <<EOF
+
+USE DNS_Project;
+SELECT id FROM domain;
+
+EOF
+)
+
+while IFS= read -r LINE
+do
+
+    NAME_CUT=$(echo $LINE | awk '{print $1}')
+    TTL_CUT=$(echo $LINE | awk '{print $2}')
+    TYPE_CUT=$(echo $LINE | awk '{print $(NF-1)}')
+    VALUE_CUT=$(echo $LINE | awk '{print $(NF-2)}')
+
+mysql --user=$DB_USER --password=$DB_PASSWD $DB_NAME -s <<EOF
+
+USE DNS_Project;
+INSERT INTO record (\`domain_id\`,\`name\`,\`type\`,\`value\`,\`ttl\`) VALUES ('$DOMAIN_ID','$NAME_CUT','$TYPE_CUT','$VALUE_CUT','$TTL_CUT');
+
+EOF
+
+done < "$FILE"
